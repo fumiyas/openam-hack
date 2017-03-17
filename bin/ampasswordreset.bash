a0b863309162a6a39877703f04e3103b9893af2f
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2017-03-18 00:56:30 +0900

    ampasswordreset: Add --ldap-uri, --name, --disable-password options

--- bin/ampasswordreset.bash
+++ bin/ampasswordreset.bash
@@ -17,16 +17,78 @@ set -o pipefail
 export LC_ALL="C"
 export PATH="/bin:/usr/bin:/opt/osstech/bin"
 
-ldap_uri="ldap://localhost:50389/"
+pdie() { echo "$0: ERROR: ${1-}" 1>&2; exit "${2-1}"; }
 
-if [[ $# -ne 3 ]]; then
-  echo "Usage: $0 BIND_DN BIND_PW_FILE AMADMIN_PW_FILE"
+ldap_uri="ldap://localhost:50389/"
+admin_name="amAdmin"
+disable_password=""
+usage="Usage: $0 [OPTIONS] BIND_DN BIND_PW_FILE [AMADMIN_PW_FILE]"
+help="$usage
+
+Options [default]:
+  -H, --ldap-uri URI
+    LDAP server (OpenDJ) URI [$ldap_uri]
+  -n, --name NAME
+    Username to reset its password [$admin_name]
+  -D, --disable-password
+    Disable password
+
+Arguments:
+  BIND_DN
+    Distinguished name to bind to the LDAP server
+  BIND_PW_FILE
+    Filename to read the bind password
+  AMADMIN_PW_FILE
+    Filename to read the new password for the target user
+    (Read stdin if no --disable-password and this argument)
+
+Examples:
+  Reset amAdmin's password:
+    $ ${0##*/} 'cn=Directory Manager' DirectoryManager.password amAdmin.password
+  Disable UrlAccessAgent's password:
+    $ ${0##*/} -n amService-URLAccessAgent -D 'cn=Directory Manager' DirectoryManager.password
+"
+
+while [[ $# -gt 0 ]]; do
+  opt="$1"; shift
+
+  case "$opt" in
+  -h|--help)
+    echo "$help"
+    exit 1
+    ;;
+  -H|--ldap-uri)
+    [[ $# -lt 1 ]] && pdie "Option requires an argument: $opt"
+    ldap_uri="$1"; shift
+    ;;
+  -n|--name)
+    [[ $# -lt 1 ]] && pdie "Option requires an argument: $opt"
+    admin_name="$1"; shift
+    ;;
+  -D|--disable-password)
+    disable_password="set"
+    ;;
+  --)
+    break
+    ;;
+  -*)
+    pdie "Invalid option: $opt"
+    ;;
+  *)
+    set -- "$opt" ${1+"$@"}
+    break
+    ;;
+  esac
+done
+
+if [[ $# -lt 2 ]]; then
+  echo "$usage"
   exit 1
 fi
 
 bind_dn="$1"; shift
 bind_pw_file="$1"; shift
-admin_pw_file="$1"; shift
+admin_pw_file="${1-}"; ${1+shift}
 
 ldap_suffix=$(
   ldapsearch \
@@ -41,16 +103,20 @@ ldap_suffix=$(
   ;
 ) || exit $?
 
-amadmin_dn="ou=amAdmin,ou=users,ou=default,ou=GlobalConfig,ou=1.0,ou=sunIdentityRepositoryService,ou=services,$ldap_suffix"
+amadmin_dn="ou=$admin_name,ou=users,ou=default,ou=GlobalConfig,ou=1.0,ou=sunIdentityRepositoryService,ou=services,$ldap_suffix"
 
-amadmin_pw_encrypted=$(
-  head -n 1 "$admin_pw_file" \
-  |tr -d '\n' \
-  |openssl dgst -sha1 -binary \
-  |openssl base64 \
-  |ampassword --encrypt /dev/stdin \
-  ;
-) || exit $?
+if [[ -n $disable_password ]]; then
+  amadmin_pw_encrypted="XXXXXXXX_PASSWORD_IS_DISABLED_XXXXXXXX"
+else
+  amadmin_pw_encrypted=$(
+    head -n 1 "${admin_pw_file:-/dev/stdin}" \
+    |tr -d '\n' \
+    |openssl dgst -sha1 -binary \
+    |openssl base64 \
+    |ampassword --encrypt /dev/stdin \
+    ;
+  ) || exit $?
+fi
 
 amadmin_sunkeyvalues=$(
   ldapsearch \
@@ -78,6 +144,6 @@ cat <<EOF \
 dn: $amadmin_dn
 changetype: modify
 replace: sunKeyValue
-$amadmin_sunkeyvalues
 sunKeyValue: userPassword=$amadmin_pw_encrypted
+$amadmin_sunkeyvalues
 EOF
